bbdc70ce8b44369494b074517e57909f4dec712f
Wenchuan <111623150+WenchuanLiliZhao@users.noreply.github.com>
2024-06-14 23:05:06 +0800

    Update Stories.tsx

--- src/Pages/Stories/Stories.tsx
+++ src/Pages/Stories/Stories.tsx
@@ -1,4 +1,4 @@
-import NBL_Story1 from "./Story1/0_Repo";
+import NBL_Story1 from "./Story_TheLightOfCompassionBeyondHell/0_Repo";
 import NBL_Story2 from "./Story2/0_Repo";
 import NBL_Story3 from "./Story3/0_Repo";
 import NBL_Story4 from "./Story4/0_Repo";
