d468030b59ce7adffbcf5ce7ba1f028ec6e60a05
Wenchuan <111623150+WenchuanLiliZhao@users.noreply.github.com>
2024-06-16 14:33:08 +0800

    test

--- src/Pages/Stories/Story2/Info.tsx
+++ src/Pages/Stories/Story2/Info.tsx
@@ -3,17 +3,17 @@ import Books from "../../Books/Books"
 import Editors from "../../Editors/Editors"
 
 const Info = {
-  title: "奇妙的兒童世界",
+  title: "《幾何原本》的中國之旅",
 
   update: [2023,1,10],
 
-  key: `story2`,
-  cover: "https://i.pinimg.com/564x/2b/0b/e6/2b0be69cdd2cef9f985c59ff903d6403.jpg",
+  key: `a-journey-of-elements-in-china`,
+  cover: "https://i.pinimg.com/originals/86/b7/6b/86b76b21b24f6f7f9b45374a4a2ab695.jpg",
 
   authors: [
     {
-      data: Editors.Editor_ShaoQianwen,
-      role: "撰文"
+      data: Editors.Editor_ZhaoWenchuan,
+      role: "作者"
     },
   ],
 
@@ -26,7 +26,7 @@ const Info = {
     Arts.Art_Art1
   ], // 不能没有
 
-  brief: "總結來說，敦煌不僅是古代絲綢之路上重要的貿易和文化中心，現在更是一座融合了歷史、藝術與自然美景的城市，繼續在現代大放異彩，吸引著無數人的目光和心靈。探索敦煌，就是在與歷史對話，感受人類文明的磅礡力量。",
+  brief: "明朝時期，中國嚴格的海禁政策對沿海民眾經濟造成重大影響，並引發大量海盜活動。1583年，利瑪竇帶來歐幾里德的《幾何原本》，與徐光啟合作將其翻譯成中文，對明代的工程發展產生了深遠影響。徐光啟在數學、天文和農業領域做出了重要貢獻，體現了中國學者對西方科技的積極學習態度。然而，明清之際的政治動蕩使這些科技文化交流的成果未能持續，最終在清朝的閉關鎖國政策下被中斷。",
 }
 
 export default Info
