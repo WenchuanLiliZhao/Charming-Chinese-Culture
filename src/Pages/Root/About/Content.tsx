83e88e3b0e87ed013ea60de2c502e6341242a567
Wenchuan <111623150+WenchuanLiliZhao@users.noreply.github.com>
2024-07-10 17:30:05 +0800

    u

--- src/Pages/Root/About/Content.tsx
+++ src/Pages/Root/About/Content.tsx
@@ -1,28 +1,64 @@
 
 import Article from "../../../Components/Article/Article";
+import NGrid from "../../../Components/Cards/InArticle/NGrid";
 
 import RootLayout from "../RootLayout";
 import Info from "./Info";
 
 export default function Content() {
+
+  const imgStyle = {border: "1px solid rgba(0, 0, 0, 0.12)", borderRadius: "16px"}
+
   return (<>
     <RootLayout info={Info} align="left">
       <Article>
-        <figure>
-          <img src="https://cdn.kastatic.org/ka-perseus-images/e0fca29dcbc591ceb49688397e031cb0abf461ee.jpg" alt="" />
 
-          <figcaption>
-            圖注寫在這裡
-          </figcaption>
-        </figure>
+        <p>「中華⽂化有意思」是⼀個以數字科技之⼒向全球傳播中華⽂化的品牌。我們順應WEB3.0時代傳播新特徵，線上線下結合，通過紙本書、電⼦書、有聲書、視頻、AR/VR/MR、⽂創等多種形態，向大眾尤其是青少年群體展現中華⽂化的美好和趣味，激發受眾帶著驚嘆與樂趣學習了解中華⽂化的豐富內涵。從經典詩詞到唐宋漢服；從書畫藝術到⾮遺⼿⼯；從器物雅玩到草⽊⿂蟲，我們⽤技術展現中華⽂化有意思的地方，由有意思⽽有內涵，由有內涵⽽致久遠。</p>
 
-        <p>歡迎來到「中華文化有意思」！我們致力於創造一個充滿智慧和愉悅的平台，以滿足您對知識和心靈需求的嚮往。在這裡，我們相信知識的力量能夠帶來內心的豐富與充實，就如同陶淵明所言：「好讀書，不求甚解；每有會意，便欣然忘食。」我們堅信，這種純粹而真實的學習樂趣才能帶來真正的滿足。</p>
+        
 
-        <p>我們的團隊集合了來自各個領域的專業人士，他們秉持著「腹有詩書氣自華」的理念，致力於傳遞有價值的內容，讓每一位讀者在瀏覽我們的網站時，都能感受到「運筆寫心，浩然自得」的精神愉悅。在這個快節奏的世界裡，我們希望能為您提供一片寧靜的閱讀天地。</p>
+        <h2>社交媒體 & App</h2>
+        
+        <NGrid columnsCount={3}>
+          <figure>
+            <img style={imgStyle} src="https://i.pinimg.com/originals/31/f2/04/31f2049f76297fd8649f37cba4c8fd85.png" alt="" />
 
-        <p>在我們的網站，您可以隨時隨地查閱豐富的資料、深入的分析和實用的建議，不論是在夜深人靜的時刻讀書，還是忙碌閒暇之餘瀏覽資訊，我們都希望能帶給您「三日不讀書，便覺語言無味」的體驗與快樂。</p>
+            <figcaption style={{textAlign: "center"}} >中華文化有意思 Facebook</figcaption>
+          </figure>
 
-        <p>來到這裡，感受知識的力量與閱讀的美好，讓我們一同在精神世界中探求無限的可能性。</p>
+          <figure>
+            <img style={imgStyle} src="https://i.pinimg.com/originals/82/bb/ad/82bbad605465800b0039c9f7bb3a6482.jpg" alt="" />
+
+            <figcaption style={{textAlign: "center"}} >知书HK微信公眾號</figcaption>
+          </figure>
+
+          <figure>
+            <img style={imgStyle} src="https://i.pinimg.com/originals/ac/06/23/ac06234cb2bd5aeb19ae438107a1e065.png" alt="" />
+
+            <figcaption style={{textAlign: "center"}} >知書 Instagram</figcaption>
+          </figure>
+
+          <figure>
+            <img style={imgStyle} src="https://i.pinimg.com/originals/f9/e4/db/f9e4dbade47f4c6ca572074cabd406a8.jpg" alt="" />
+
+            <figcaption style={{textAlign: "center"}} >知書APP下載</figcaption>
+          </figure>
+
+          <figure>
+            <img style={imgStyle} src="https://i.pinimg.com/736x/78/17/8b/78178b74f39469561cc54440c332092a.jpg" alt="" />
+
+            <figcaption style={{textAlign: "center"}} >知書閱聽圖書館 Facebook</figcaption>
+          </figure>
+        </NGrid>
+
+        <hr />
+
+        <h2>聯繫方式</h2>
+
+        <p><span className="material-symbols-outlined">mail</span> cs@suep.com</p>
+
+        <p><span className="material-symbols-outlined">call</span> 98249587 / 25978415</p>
+
       </Article>
     </RootLayout>
   </>)
